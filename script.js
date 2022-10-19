8ace9980c08657b6e771946e866b0cdd2579a88c
luka gulverdashvili <109980584+Gulverda@users.noreply.github.com>
2022-10-19 22:29:43 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const header = document.querySelector("header");
+const loader = document.querySelector('.loader');
+const fullweb = document.querySelector('.fullweb');
+
+function init(){
+    setTimeout(() => {
+        loader.style.opacity = 0;
+        loader.style.display = 'none';
+
+        fullweb.style.display = 'block';
+        setTimeout(() => (fullweb.style.opacity = 1), 50);
+    }, 8000);
+}
+
+init();
+
+window.addEventListener ("scroll", function() {
+    header.classList.toggle ("sticky", window.scrollY > 0);
+});
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('open');
+};
